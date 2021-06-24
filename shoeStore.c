9e4b3c68ad23099a0d80f44512b852ef11d641ec
Conraduouou <louiserafaellalu@gmail.com>
2021-06-24 21:34:08 +0800

    Finish enterRecord function and as well as utility functions
    I was able to pull through from start of lunch to dinnertime to finish
    the needed functions, although a little bit sloppily. If I was going
    to be the judge of such code, I think I'll be jumping here and there
    to understand the gist of the whole enterRecord function.
    
    I've also finished utility functions such as checking what stock is
    currently recorded or not recorded, if there are already pairs in the
    stock, or if there's already a style number in the records. I'm going
    to try to refactor the code to make it recursive if I still have time
    after this.

--- shoeStore.c
+++ shoeStore.c
@@ -12,21 +12,23 @@ struct shoes
 struct stockShoes
 {
     int styleNo;
-    struct shoes shoesBySize[11];   // since sizes are ranged 3 - 14
     float price;
+    bool recorded;
+    struct shoes shoesBySize[11];   // since sizes are ranged 3 - 14
 };
 
 // record of shoes in stock
 struct stockShoes stock[10];
+
+// reset purposes
+struct stockShoes reset;
 
 // assign records function
 void assignStart(struct stockShoes stock[], int n)
 {
     for (int i = 0; i <= n; i++)
     {
-        do
-            stock[i].styleNo = rand() % 51;
-        while (checkExistingStyle(stock, stock[i].styleNo));
+        stock[i].recorded = false;
 
         for (int j = 0; j < 11; j++)
             stock[i].shoesBySize[j].size = j + 3;
@@ -37,17 +39,66 @@ void assignStart(struct stockShoes stock[], int n)
 // returns true if there is, false if there is not
 bool checkExistingStyle(struct stockShoes stock[], int styleNumber)
 {
-    for (int i = 0; i < NUMBER_OF_STOCK; i++)
-        if (stock[i].styleNo == styleNumber)
+    if (styleNumber < 0 || styleNumber > 50)
+        return true;
+    else
+        for (int i = 0; i < NUMBER_OF_STOCK; i++)
+            if (stock[i].styleNo == styleNumber)
+                return true;
+    
+    return false;
+}
+
+// function that checks if there is already pairs in the stock's
+// different shoe sizes
+bool checkExistingPairs(struct stockShoes stock)
+{
+    for (int i = 0; i < 11; i++)
+        if (stock.shoesBySize[i].noOfPairs != 0)
             return true;
     
     return false;
 }
 
-// function that lets user enter a record on current stock
-void enterRecord()
+// function that checks last recorded stock and returns stock
+// number after it, otherwise, returns -1 if all stocks are occupied
+int checkExistingStock(struct stockShoes stock[])
+{
+    for (int i = 0; i < NUMBER_OF_STOCK; i++)
+        if (!stock[i].recorded)
+            return i;
+    
+    return -1;
+}
+
+// print function that treats its second parameter either as stock index
+// or style number depending on the third boolean parameter
+// if true, n is stock index
+// if false, n is style number/code
+void printStock(struct stockShoes stock[], int n, bool isStock)
+{
+    int index = n;
+
+    if (!isStock)
+        for (int i = 0; i < NUMBER_OF_STOCK; i++)
+            if (stock[i].styleNo == n)
+            {
+               index = i;
+               break;
+            }
+    
+    printf("Shoe stock number: %2d\n", index);
+    printf("Shoe style number: %2d\n", stock[index].styleNo);
+}
+
+// function that changes record on stack either through stock or style
+// a collective function that changes price or number of items in stock on hand
+// depending on value of boolean isPrice
+// changes price if true, changes number if false
+void change(bool isPrice)
 {
     int userChoice;
+    int stockChoice;
     do
     {
         system("cls");
@@ -62,9 +113,151 @@ void enterRecord()
     } while (userChoice < 1 || userChoice > 3);
 
 
-    
+    if (userChoice == 1)
+    {
+        do
+        {
+            system("cls");
+
+            printf("Stock is numbered 0 to %d\n\n", NUMBER_OF_STOCK - 1);
+            printf("Enter a stock number: ");
+            scanf(" %d", &stockChoice);
+        } while (stockChoice < 0 || stockChoice > 9);
+    }
 }
 
+// function that lets user enter a record on current stock
+void enterRecord()
+{
+    int userChoice;
+    int stockToInsert = checkExistingStock(stock);
+    int temp;
+    float ftemp;
+
+    bool styleFinished  = false;
+    bool priceFinished  = false;
+    bool sizeFinished   = false;
+    bool numberFinished = false;
+
+    while (true)
+    {
+        system("cls");
+
+        printf("This is stock number %d\n\n", stockToInsert);
+        printf("Enter -1 to go back to main menu.\n");
+        if (styleFinished)
+            printf("Enter -2 to go back.\n");
+
+        // print if already supplied
+        if (styleFinished)
+            printf("\nShoe's current style: %d\n", stock[stockToInsert].styleNo);
+        if (priceFinished)
+            printf("Shoe's current price: %.2f\n", stock[stockToInsert].price);
+        if (sizeFinished || checkExistingPairs(stock[stockToInsert]))
+            for (int i = 0; i < 11; i++)
+                if (stock[stockToInsert].shoesBySize[i].noOfPairs != 0)
+                    printf("Shoe size %2d's pairs: %d\n", i + 3, stock[stockToInsert].shoesBySize[i].noOfPairs);
+        
+        // supplying process
+        if (!styleFinished)
+        {
+            printf("\n\n\n\n\n\nStyle number is within range 0 to 50 inclusive.\n");
+            printf("Enter style number: ");
+            scanf(" %d", &temp);
+
+            if (!checkExistingStyle(stock, temp))
+            {
+                stock[stockToInsert].styleNo = temp;
+                styleFinished = true;
+            }
+            else if (temp == -1)
+                break;
+        }
+
+        else if (!priceFinished)
+        {
+            printf("\n\n\nPrice should be above 0.\n");
+            printf("Enter price: ");
+            scanf(" %f", &ftemp);
+
+            temp = (int) ftemp;
+
+            if (ftemp > 0)
+            {
+                stock[stockToInsert].price = ftemp;
+                priceFinished = true;
+            }
+            else if (temp == -1)
+                break;
+            else if (temp == -2)
+            {
+                stock[stockToInsert].styleNo = 0;
+                styleFinished = false;
+            }
+        }
+
+        else if (!sizeFinished)
+        {
+            printf("\n\nSizes are within range of 3 to 14 inclusive.\n");
+            printf("Enter size to add: ");
+            scanf(" %d", &temp);
+
+            if (temp >= 3 && temp <= 14)
+            {
+                int temp2;
+                struct shoes *tempshoes = &stock[stockToInsert].shoesBySize[temp - 3];
+
+                printf("Enter number of pairs in shoe size %d: ", tempshoes->size);
+                scanf(" %d", &temp2);
+
+                if (temp2 > 0)
+                {
+                    tempshoes->noOfPairs = temp2;
+
+                    do
+                    {
+                        system("cls");
+
+                        printf("Would you like to add more pairs in other sizes?\n");
+                        printf("1 - for yes\n");
+                        printf("2 - for no\n\n");
+
+                        printf("Enter choice: ");
+                        scanf(" %d", &temp2);
+                    } while (temp2 < -2 || temp2 > 2 || temp2 == 0);
+
+
+                    if (temp2 == 2)
+                    {
+                        sizeFinished = true;
+                        stock[stockToInsert].recorded = true;
+                        break;
+                    }
+                    else if (temp2 == -1)
+                        break;
+                    else if (temp2 == -1)
+                        priceFinished = false;
+                }
+                else if (temp2 == -1)
+                    break;
+                else if (temp2 == -2)
+                    priceFinished = false;
+            }
+            else if (temp == -1)
+                break;
+            else if (temp == -2)
+                priceFinished = false;
+        }
+    }
+
+    if (!stock[stockToInsert].recorded)
+        stock[stockToInsert] = reset;
+}
+
+void displayRecord()
+{
+
+}
 
 // menu function
 void inventory()
